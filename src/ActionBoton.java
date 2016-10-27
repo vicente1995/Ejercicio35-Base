f7453f9079d0c5d99a235a31bbbaa8caecea3fe3
Jesús Redondo García <redondogarciajesus@gmail.com>
2016-10-27 16:19:57 +0200

    Añadir *

--- src/ActionBoton.java
+++ src/ActionBoton.java
@@ -7,7 +7,7 @@ import java.awt.event.ActionListener;
  * Se puede lograr pasando en el constructor la referencia a la ventana.
  * Recuerda que desde la ventana, se puede acceder a la variable de tipo ControlJuego
  * @author jesusredondogarcia
- *
+ **
  */
 public class ActionBoton implements ActionListener{
 
